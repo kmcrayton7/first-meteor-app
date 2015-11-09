0b50f7d8b6ab52f86f40468be4f65115e1ae28e5
Kim Crayton <kmcrayton7@yahoo.com>
2015-11-09 08:58:01 -0500

    "Add ability to remove players"

--- leaderboard.js
+++ leaderboard.js
@@ -30,6 +30,10 @@ if(Meteor.isClient){
         'click .decrement': function(){
             var selectedPlayer = Session.get('selectedPlayer');
             PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+        },
+        'click .remove': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.remove(selectedPlayer);
         }
     });
     Template.addPlayerForm.events({
